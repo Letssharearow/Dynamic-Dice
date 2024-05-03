8886cb223b4be0f821bff6451c366b2d1fcfc0f8
Sehne, Julian <julian.sehne@student.fhws.de>
2024-05-03 18:53:36 +0200

    Fix Dependencies?

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,10 +28,10 @@ android {
     }
   }
   compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_1_8 // TODO higher Java version?
-    targetCompatibility = JavaVersion.VERSION_1_8
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
   }
-  kotlinOptions { jvmTarget = "1.8" }
+  kotlinOptions { jvmTarget = "17" }
   buildFeatures { compose = true }
   composeOptions { kotlinCompilerExtensionVersion = "1.5.1" }
   packaging { resources { excludes += "/META-INF/{AL2.0,LGPL2.1}" } }
