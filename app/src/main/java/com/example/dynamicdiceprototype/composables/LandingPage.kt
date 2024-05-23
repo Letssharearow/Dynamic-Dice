30e8eb6575179ad8b8522789194feed94110f06f
Sehne, Julian <julian.sehne@student.fhws.de>
2024-05-23 18:33:19 +0200

    Revert "feat: Roll Dices help"
    This reverts commit 5fa30c74180a8c84a7462abd391245cfacd868fa.

--- app/src/main/java/com/example/dynamicdiceprototype/composables/LandingPage.kt
+++ app/src/main/java/com/example/dynamicdiceprototype/composables/LandingPage.kt
@@ -10,14 +10,10 @@ import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.dynamicdiceprototype.composables.common.AlertBox
 import com.example.dynamicdiceprototype.data.Dice
 import com.example.dynamicdiceprototype.services.TAG
 import com.example.dynamicdiceprototype.services.getDices
@@ -33,7 +29,6 @@ fun LandingPage(
 ) {
 
   Log.d(TAG, "Recompose LandingPage $name => $dices")
-  var openDialog by remember { mutableStateOf(true) }
 
   Column(
       verticalArrangement = Arrangement.SpaceBetween,
@@ -47,17 +42,6 @@ fun LandingPage(
         }
         DiceButtonM3(onRollClicked = onRollClicked, modifier = Modifier.padding(vertical = 16.dp))
       }
-  if (dices.none { it.current != null }) {
-    AlertBox(
-        title = "Roll Dices",
-        text = "",
-        isOpen = openDialog,
-        onDismiss = { openDialog = false },
-        conConfirm = {
-          openDialog = false
-          onRollClicked()
-        })
-  }
 }
 
 @Preview(showBackground = true)
