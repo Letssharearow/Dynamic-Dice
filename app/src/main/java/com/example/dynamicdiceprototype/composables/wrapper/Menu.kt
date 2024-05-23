d8c19ee719ec7782beb95cdb15b63297e872a835
Sehne, Julian <julian.sehne@student.fhws.de>
2024-05-23 13:35:15 +0200

    Mage menu nicer

--- app/src/main/java/com/example/dynamicdiceprototype/composables/wrapper/Menu.kt
+++ app/src/main/java/com/example/dynamicdiceprototype/composables/wrapper/Menu.kt
@@ -1,14 +1,23 @@
 package com.example.dynamicdiceprototype.composables.wrapper
 
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Create
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.rounded.Create
 import androidx.compose.material3.DrawerState
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.NavigationDrawerItemDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -20,16 +29,18 @@ import com.example.dynamicdiceprototype.services.HeaderViewModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
+data class NavItmProps(val text: String, val route: String, val imageVector: ImageVector)
+
 @Composable
 fun Menu(drawerState: DrawerState, scope: CoroutineScope) {
   val navController = rememberNavController()
   val navItems =
       listOf(
-          Pair("Test Screen", Screen.TestScreen.route),
-          Pair("Main Screen", Screen.MainScreen.route),
-          Pair("Dices", DicesScreen.Dices.route),
-          Pair("Dice Groups", Screen.DiceGroups.route),
-          Pair("Upload Image", Screen.UploadImage.route),
+          NavItmProps("Test Screen", Screen.TestScreen.route, Icons.Default.Info),
+          NavItmProps("Main Screen", Screen.MainScreen.route, Icons.Default.Home),
+          NavItmProps("Dices", DicesScreen.Dices.route, Icons.Default.Create),
+          NavItmProps("Dice Groups", Screen.DiceGroups.route, Icons.Filled.Create),
+          NavItmProps("Upload Image", Screen.UploadImage.route, Icons.Rounded.Create),
       )
 
   ModalNavigationDrawer(
@@ -38,7 +49,7 @@ fun Menu(drawerState: DrawerState, scope: CoroutineScope) {
         ModalDrawerSheet {
           Text("Navigate", fontWeight = FontWeight(550), modifier = Modifier.padding(16.dp))
           HorizontalDivider()
-          navItems.forEach { NavItem(navController, scope, drawerState, it.first, it.second) }
+          navItems.forEach { NavItem(navController, scope, drawerState, it) }
         }
       }) {
         NavGraph(navController)
@@ -50,17 +61,23 @@ fun NavItem(
     navController: NavHostController,
     scope: CoroutineScope,
     drawerState: DrawerState,
-    name: String,
-    route: String
+    props: NavItmProps,
 ) {
   val viewModel = viewModel<HeaderViewModel>()
 
   NavigationDrawerItem(
-      label = { Text(text = name) },
+      icon = {
+        Icon(
+            imageVector = props.imageVector,
+            contentDescription = null,
+            tint = MaterialTheme.colorScheme.onSurface)
+      },
+      label = { Text(text = props.text) },
       selected = false,
       onClick = {
-        viewModel.changeHeaderText(name)
-        navController.navigate(route)
+        viewModel.changeHeaderText(props.text)
+        navController.navigate(props.route)
         scope.launch { drawerState.close() }
-      })
+      },
+      modifier = Modifier.padding(NavigationDrawerItemDefaults.ItemPadding))
 }
