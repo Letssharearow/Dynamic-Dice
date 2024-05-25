504cf67106ac43b4493073a61ced0fb25709f48d
Sehne, Julian <julian.sehne@student.fhws.de>
2024-05-25 09:28:57 +0200

    comment uploadImages

--- app/src/main/java/com/example/dynamicdiceprototype/MainActivity.kt
+++ app/src/main/java/com/example/dynamicdiceprototype/MainActivity.kt
@@ -29,7 +29,6 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.dynamicdiceprototype.composables.wrapper.Menu
 import com.example.dynamicdiceprototype.services.HeaderViewModel
 import com.example.dynamicdiceprototype.ui.theme.DynamicDicePrototypeTheme
-import com.example.dynamicdiceprototype.utils.uploadImages
 import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
@@ -38,7 +37,7 @@ class MainActivity : ComponentActivity() {
     val res = resources
     //    uploadUser()
     //    uploadDices()
-    uploadImages(res)
+    //    uploadImages(res)
     //    val firebase = FirebaseDataStore()
 
     setContent {
