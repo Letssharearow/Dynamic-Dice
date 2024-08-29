7b60a943ed8548b503d68faecd7e972d3ae9de18
Sehne, Julian <julian.sehne@student.fhws.de>
2024-08-29 08:12:49 +0200

    UNDO height

--- app/src/main/java/com/example/dynamicdiceprototype/composables/ItemListScreen.kt
+++ app/src/main/java/com/example/dynamicdiceprototype/composables/ItemListScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -101,6 +102,8 @@ fun <T> ItemListScreen(
       }
     }
     if (onRandomItem != null || onCreateItem != null) {
+      Spacer(modifier = modifier.height(500.dp))
+
       Row(
           Modifier.fillMaxWidth()
               .background(Color.Transparent)
